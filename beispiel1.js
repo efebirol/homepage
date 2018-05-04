b80565b59f740ec0f5e818274adb8f26c6edfabc
Birol <b.efe@compuccino.com>
2018-05-04 10:27:28 +0200

    Node SCSS (Sass) script und neues SCSS Datei

---
+++ beispiel1.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+//Skript "beispiel1.js" einfach ausführen über "node beispiel1", ohne .js Endung
+console.log("Usage: im Terminal eingeben ‘node beispiel1'");
+
+console.log("Text ohne Einfaerbung.'");
+
+console.log(chalk.magenta("Text MIT chalk Einfaerbung!'"));
+
